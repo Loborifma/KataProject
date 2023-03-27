b11c6c7591fe53b52a1531806f87975dd9798d4a
Max <max.f29@mail.ru>
2023-03-28 01:26:52 +0400

    .

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import '../../node_modules/focus-visible/dist/focus-visible';
 import '../scss/main.scss';
-import '../index.html';
+import '../../index.html';
 import './scripts/burger-menu';
 import './scripts/check-up__modal';
 import './scripts/show-more__services';
